911b05f2552d0bc4e0db6bb3dc873ede94930075
Valentin Permiakov <valentin.permiakov92@gmail.com>
2023-12-18 15:15:20 +0100

    fixed type error in GET

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,6 @@ import {
   getLowestPrice,
 } from '@/lib/utils';
 import { User } from '@/types';
-import { NextResponse } from 'next/server';
 
 export const maxDuration = 10; // 10 seconds
 export const dynamic = 'force-dynamic';
@@ -22,21 +21,21 @@ export async function GET(request: Request) {
 
     const products = await Product.find({});
 
-    if (!products) {
-      throw new Error('No Products found');
-    }
+    if (!products) throw new Error('No product fetched');
 
-    // 1. Scrape latest product details and update DB
-
+    // 1 SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
     const updatedProducts = await Promise.all(
       products.map(async (currentProduct) => {
+        // Scrape product
         const scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
 
-        if (!scrapedProduct) throw new Error('No product found');
+        if (!scrapedProduct) return;
 
         const updatedPriceHistory = [
           ...currentProduct.priceHistory,
-          { price: scrapedProduct.currentPrice },
+          {
+            price: scrapedProduct.currentPrice,
+          },
         ];
 
         const product = {
@@ -47,13 +46,15 @@ export async function GET(request: Request) {
           averagePrice: getAveragePrice(updatedPriceHistory),
         };
 
+        // Update Products in DB
         const updatedProduct = await Product.findOneAndUpdate(
-          { url: product.url },
+          {
+            url: product.url,
+          },
           product
         );
 
-        // 2. Check each product status and set email accordingly
-
+        // 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
         const emailNotifType = getEmailNotifType(
           scrapedProduct,
           currentProduct
@@ -64,13 +65,16 @@ export async function GET(request: Request) {
             title: updatedProduct.title,
             url: updatedProduct.url,
           };
-
-          const emailContent = generateEmailBody(productInfo, emailNotifType);
-
-          const userEmails = updatedProduct.users.map(
-            (user: User) => user.email
+          // Construct emailContent
+          const emailContent = await generateEmailBody(
+            productInfo,
+            emailNotifType
           );
-
+          // Get array of user emails
+          const userEmails = updatedProduct.users.map(
+            (user: any) => user.email
+          );
+          // Send email notification
           await sendEmail(emailContent, userEmails);
         }
 
@@ -82,7 +86,7 @@ export async function GET(request: Request) {
       message: 'Ok',
       data: updatedProducts,
     });
-  } catch (error) {
-    throw new Error(`Failed in GET ${error}`);
+  } catch (error: any) {
+    throw new Error(`Failed to get all products: ${error.message}`);
   }
 }
